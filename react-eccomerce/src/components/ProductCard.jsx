14e9b643a9ba553c45ada4711e8b57ef350048d0
Your Name <abhijeetkarle6@gmail.com>
2026-07-05 23:40:06 +0530

    this is

--- react-eccomerce/src/components/ProductCard.jsx
+++ react-eccomerce/src/components/ProductCard.jsx
@@ -1,9 +1,12 @@
 function ProductCard({ product }) {
   return (
     <div>
-        <div className='p-4 h-full w-full object-contain rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300'>
-          <img src={product.image} alt={product.title} width={200} />
-          <h2>{product.title}</h2>
+      <div className='p-4 h-full w-full object-contain rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300'>
+        <div className="flex justify-center">
+           <img src={product.image} alt={product.title} width={200} />
+        </div>
+
+          <h2 className="font-semibold line-clamp-2">{product.title}</h2>
           <p className='font-bold'>${product.price}</p>
           <p>{product.category}</p>
           <button className='bg-red-500 text-white px-4 py-2 rounded-lg mt-2 hover:bg-blue-600 transition-colors duration-300'>Add to Cart</button>
