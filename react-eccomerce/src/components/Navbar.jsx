e5ea0c8626c957fa9fa9d9f673dc1e56643aa116
Your Name <abhijeetkarle6@gmail.com>
2026-07-06 16:54:45 +0530

    add Logo of Site

--- react-eccomerce/src/components/Navbar.jsx
+++ react-eccomerce/src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import logo from '../assets/logo.svg';
+import StoreLogo from '../assets/StoreLogo.png';
 import { Link } from 'react-router-dom';
 function Navbar({search, setSearch, cartItems}) {
   return (
     <nav className="flex justify-between items-center bg-gray-400 text-white shadow-md px-8 py-4">
-      <div className="flex items-center gap-2">
-        <img src={logo} alt="Logo" width={50} />
+      <div className="flex items-center justify-start gap-2">
         <h1>React Store</h1>
+        <img src={StoreLogo} alt=" Store Logo" width={100} />
       </div>
       <div>
         <input type="text" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} className='border border-gray-300 rounded-lg px-4 py-2 w-96'/>
       </div>
       <div className="flex items-center gap-6">
           <Link to="/">Home</Link>
-          <Link to="/cart">Cart({cartItems.length})</Link>
+          <Link to="/cart"><img src={logo} alt="Logo" width={50} />({cartItems.length})</Link>
         <button onClick={() => alert('Login functionality not implemented yet.')}>🌙</button>
       </div>
     </nav>
